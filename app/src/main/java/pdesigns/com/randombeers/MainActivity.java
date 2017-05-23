4774df11a3ebcd5a9156d40d0534c9518352ec67
Patrick <paddy1414@live.ie>
2017-05-23 13:29:39 +0100

    Layout compleated -PN

--- app/src/main/java/pdesigns/com/randombeers/MainActivity.java
+++ app/src/main/java/pdesigns/com/randombeers/MainActivity.java
@@ -1,13 +1,30 @@
 package pdesigns.com.randombeers;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button nextBeer;
+    private TextView beerDescp, beerName;
+    private ImageView beerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beerDescp  = (TextView) findViewById(R.id.beer_descp);
+        beerName  = (TextView) findViewById(R.id.beer_title);
+        beerImage  = (ImageView) findViewById(R.id.beer_image);
+        beerDescp  = (Button) findViewById(R.id.next_beet_btn);
+
     }
+
+
+
 }
